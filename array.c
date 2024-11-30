f23ccf37dd915a629fcf2507a171db29b961f63a
fizarum <fizarum@gmail.com>
2024-11-30 20:28:31 +0100

    fixed array's size & capacity when array is null; (#12)

--- array.c
+++ array.c
@@ -30,9 +30,9 @@ void ArrayDestroy(Array_t* array) {
   free(array);
 }
 
-_u16 ArraySize(const Array_t* array) { return array->size; }
+_u16 ArraySize(const Array_t* array) { return array != NULL ? array->size : 0; }
 
-_u16 ArrayCapacity(const Array_t* array) { return array->capacity; }
+_u16 ArrayCapacity(const Array_t* array) { return array != NULL ? array->capacity : 0; }
 
 bool ArrayAdd(Array_t* array, void* value) {
   if (ArrayIsFull(array) == true) return false;
